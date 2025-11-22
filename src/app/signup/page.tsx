cd0591ee1cf22c744ddc5bdf6c6a9e600200742a
rasa <r6yuna@gmail.com>
2025-11-22 16:19:42 +0900

    Add:signup/page.tsx(作業途中)

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,20 @@
+
+export default function SignUp() {
+    return (
+        <div>
+            <div>
+                <h1>Sign Up</h1>
+            </div>
+            <div>
+                <p>Name</p>
+            </div>
+            <div>
+                <p>Email</p>
+            </div>
+            <div>
+                <p>Password</p>
+            </div>
+
+        </div>
+    );
+}
